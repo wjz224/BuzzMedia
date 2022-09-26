2ee49a68ea9407087038f91eeafb33afa8b2c278
scn224 <scn224@lehigh.edu>
2022-09-26 15:35:42 -0400

    Completed react setup tutorial

---
+++ reacttut/src/index.tsx
@@ -0,0 +1,7 @@
+import "./style.css"
+import * as React from 'react'
+import * as ReactDOM from "react-dom";
+
+const Hello = () => <h1>Hello from React</h1>;
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
